539cc71c192adc61d3a4b1880e8681df626f9474
TadicharlaChandini <tadicharlachandini@gmail.com>
2025-05-14 11:01:17 +0530

    src files

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <address id='address'>
+      Vishnu Education Society <br/>
+      Vishnupur, Bhimavaram, Andhra Pradesh - 534202
+      </address>
+      <div className='responsive-map'>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3824.140549567941!2d81.52297447519226!3d16.5694315841849!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a37cd48b6d5796b%3A0xb1dab93852add538!2sVishnu%20Education%20Society!5e0!3m2!1sen!2sin!4v1740291463099!5m2!1sen!2sin" width="600" height="450" allowFullscreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+    </>
+  )
+}
+
+export default App
